ce89b94b6205d39716c5af1a56aa59f58dc8a414
Ayush Pratap Singh <ayushdce2@gmail.com>
2024-12-26 10:07:06 +0530

    added

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+
+const My_Mongo_Url = "mongodb+srv://ayushdce2:8445315561Aa@cluster0.8kkxg.mongodb.net/todo_mern?retryWrites=true&w=majority&appName=Cluster0";
+mongoose.connect(My_Mongo_Url)
+.then((response)=>{console.log("Connected to empMgmtD"); db_status="connected DB"})
+.catch((error)=>{console.log(error,"error NOT connected to empMgmtD"); db_status="NoT connected DB"});
 
 app.get("/",(req,res)=>{
     res.send("backend Connected  . . . . .  .");
